addcbc7993ecb755d9ead3e29013b3ee5f99a044
Карина <karinkademchenko@yandex.ru>
2026-05-16 20:14:39 +0300

    Changes based on comments: add async config to request-service

--- core/request-service/src/main/java/ru/practicum/service/processors/ParticipationRequestPostProcessor.java
+++ core/request-service/src/main/java/ru/practicum/service/processors/ParticipationRequestPostProcessor.java
@@ -11,7 +11,6 @@ import ru.practicum.request.enums.EventRequestStatus;
 
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.google.protobuf.Timestamp;
 
